d75ba401f03d6fbc011e4a3b08cb064b08c6f0be
Your Name <corsiferrao@gmail.com>
2024-02-13 15:30:00 +0000

    solution ok

--- exe1/main.c
+++ exe1/main.c
@@ -3,6 +3,7 @@
 #include "hardware/gpio.h"
 
 const int LED_PIN = 5;
+const int LED_PIN_Y = 28;
 
 int main() {
     stdio_init_all();
@@ -10,11 +11,16 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+
     while (true) {
         gpio_put(LED_PIN, 1);
+        gpio_put(LED_PIN_Y, 1);
         sleep_ms(250);
         
         gpio_put(LED_PIN, 0);
+        gpio_put(LED_PIN_Y, 0);     
         sleep_ms(250);
     }
 }
--- exe4/main.c
+++ exe4/main.c
@@ -1,5 +1,14 @@
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+
+const int BTN_PIN_R = 28; 
+
+const int LED_PIN_R = 5;
+const int LED_PIN_P = 8; 
+const int LED_PIN_B = 11;
+const int LED_PIN_Y = 15; 
 
 int main() {
   stdio_init_all();
@@ -7,11 +16,64 @@ int main() {
   // configurar botao como entrada
   // lembre de ativar o pullup!
 
-  int btn_counter = 0;
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+  gpio_init(LED_PIN_P);
+  gpio_set_dir(LED_PIN_P, GPIO_OUT);
+
+  gpio_init(LED_PIN_B);
+  gpio_set_dir(LED_PIN_B, GPIO_OUT);
+
+  gpio_init(LED_PIN_Y);
+  gpio_set_dir(LED_PIN_Y, GPIO_OUT);  
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
   while (true) {
 
-    // lembre de fazer o debouncing antes!
-    // toda vez que o botao for apertado fazer esse print
-    printf("Button pressed %d times \n", ++btn_counter);
+    if (!gpio_get(BTN_PIN_R)) {
+      while (!gpio_get(BTN_PIN_R)){}
+
+      gpio_put(LED_PIN_R, 1);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 0);
+      gpio_put(LED_PIN_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 1);
+      gpio_put(LED_PIN_B, 0);
+      gpio_put(LED_PIN_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 1);
+      gpio_put(LED_PIN_Y, 0);
+      sleep_ms(300);
+
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 1);
+      gpio_put(LED_PIN_Y, 0);
+      sleep_ms(300);   
+
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 0);
+      gpio_put(LED_PIN_Y, 1);
+      sleep_ms(300);         
+
+      gpio_put(LED_PIN_R, 0);
+      gpio_put(LED_PIN_P, 0);
+      gpio_put(LED_PIN_B, 0);
+      gpio_put(LED_PIN_Y, 0);
+      sleep_ms(100); 
+    }
+
+
   }
 }
