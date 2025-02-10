adc4442e215b6a0fd526980fe82af93ec27aa51a
Your Name <corsiferrao@gmail.com>
2025-02-10 10:58:53 -0300

    fix exe5

--- exe5/main.c
+++ exe5/main.c
@@ -21,13 +21,13 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
-            printf("Botao 0: %d\n", cnt_1++);
+            printf("Botao 1: %d\n", cnt_1++);
             sleep_ms(200);
             while(!gpio_get(BTN_PIN)) {sleep_ms(50);}
         }
 
         if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 1: %d\n", cnt_2++);
+            printf("Botao 2: %d\n", cnt_2++);
             sleep_ms(200);
             while(!gpio_get(BTN_PIN_2))  {sleep_ms(50);}
         }
