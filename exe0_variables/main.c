6e1776848fb2bf8366d2ab5fbacd0b3ce0c0ec7a
Rafael Corsi <rafael.corsi@insper.edu.br>
2025-08-15 12:53:10 -0300

    Update main.c

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
 
-#define NUM_ELEMENTS 5
+int a = 0; // var global
+int b = 0; // var global
 
-int multiplyNumbers(int x, int y) {
-    int product = x * y;
+void foo(void) {
+    a = a + 1; // acessa variavel global
 }
 
-int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
-    int result = multiplyNumbers(a, b);
-
-    int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
-        arr[i] = i;
+void main(void) {
+    while (1) {
+        foo();
+        
+        if (a > 5) {
+            b = 1;
+        }
     }
-
-    return 0;
 }
