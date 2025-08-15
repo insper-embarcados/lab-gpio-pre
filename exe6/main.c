fda499c43365a02162d8f44f36ce2728cdd91581
Marco Mello <marcoasma@insper.edu.br>
2025-08-15 12:49:56 -0300

    exe6 main

--- exe6/main.c
+++ exe6/main.c
@@ -6,8 +6,7 @@
 int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
-int BUTTON_GPIO;
-int cnt;
+
 int last_btn; // Button not pressed (pulled up)
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
@@ -31,7 +30,7 @@ void seven_seg_init() {
     }
 }
 
-void seven_seg_display() {
+void seven_seg_display(int cnt) {
     int value = bits[cnt];
     for (int i = 0; i < 7; i++) {
         int gpio = FIRST_GPIO + i;
@@ -43,15 +42,15 @@ void seven_seg_display() {
 int main() {
     stdio_init_all();
     int aux = 0;
-
-    BUTTON_GPIO = FIRST_GPIO + 7;
+    int cnt;
+    int BUTTON_GPIO = FIRST_GPIO + 7;
 
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
     seven_seg_init();
-    seven_seg_display(2);
+    seven_seg_display(0);
 
     while (true) {
         int btn = gpio_get(BTN_PIN_G);
@@ -59,7 +58,7 @@ int main() {
             if (++cnt > 9) {
                 cnt = 0;
             }
-            seven_seg_display();
+            seven_seg_display(cnt);
             printf("cnt: %d\n", cnt);
         }
         last_btn = btn;
