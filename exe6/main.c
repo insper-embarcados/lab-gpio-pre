71ae4576dee79f2fbe5bfe039c936f970ff44b51
Corsi <corsiferrao@gmail.com>
2025-08-15 11:51:30 -0300

    update test 6

--- exe6/main.c
+++ exe6/main.c
@@ -7,7 +7,7 @@ int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
 int BUTTON_GPIO;
-volatile int cnt;
+int cnt;
 int last_btn; // Button not pressed (pulled up)
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
